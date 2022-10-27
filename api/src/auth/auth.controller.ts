4ae62b6f07ee16ac9a201c80b5b1af9b6ae8345f
fkymy <yuskefuku@gmail.com>
2022-10-27 21:35:58 +0900

    add prisma

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Post } from '@nestjs/common'
+import { Controller, Post, Body, ParseIntPipe } from '@nestjs/common'
 import { AuthService } from './auth.service'
+import { AuthDto } from './dto'
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Body() dto: AuthDto) {
     return this.authService.signup()
   }
 
--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable({})
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
   test() {
     console.log('test')
   }
