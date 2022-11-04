8b3afcd0048849cddbc236e8abdf2f9b660b095a
fkymy <yuskefuku@gmail.com>
2022-11-05 05:16:51 +0900

    create relayed orders

--- backend/src/admin/admin.service.ts
+++ backend/src/admin/admin.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { createClient, getClient } from '@reservoir0x/reservoir-kit-client'
+import * as Sdk from '@reservoir0x/sdk'
 import { Network, Alchemy } from 'alchemy-sdk'
 import axios from 'axios'
+import pLimit from 'p-limit'
+import { logger } from 'src/logger'
+import { LooksRare, LooksRareOrder } from 'src/looksrare'
 import { PrismaService } from 'src/prisma/prisma.service'
 
 export const constAddress = {
@@ -22,12 +26,6 @@ export class AdminService {
   sdk: any
   testApiKey = 'demo-api-key'
   testCollectionAddress = '0x0bacc0e4fb3fe96b33d43b20a2f107f6cea31741'
-  testCollectionSetId =
-    '5c93e3b29f2072a7d4206fb9c5602376822c602843186f05846d7d0c3dbe3ccf'
-
-  collectionAddress = '0x629a673a8242c2ac4b7b8c5d8735fbeac21a6205'
-  collectionSet =
-    'f7255476e3a86310e688307089d85d4a73c05b64642bd2f0a82f5ca91e8cbeb0'
 
   constructor(private prisma: PrismaService, private config: ConfigService) {
     createClient({
@@ -58,8 +56,74 @@ export class AdminService {
     return data
   }
 
+  async fetchOrders(
+    lastSyncedHash = '',
+    cursor = '',
+    startTime = 0,
+    endTime = 0,
+    backfill = false,
+  ) {
+    logger.info('fetch_orders_looksrare', 'test Fetching orders from LooksRare')
+
+    return ['', '']
+  }
+
   async testRelayer() {
-    return 'relayer'
+    const looksRare = new LooksRare()
+
+    const url = looksRare.buildFetchOrdersUrl({ startTime: 0, endTime: 0 })
+
+    try {
+      const response = await axios.get(url, {
+        timeout: 10000,
+      })
+      console.log(response.data.data)
+      const orders: LooksRareOrder[] = response.data.data
+      const parsedOrders: Sdk.LooksRare.Order[] = []
+
+      const values: any[] = []
+
+      const handleOrder = async (order: LooksRareOrder) => {
+        const orderTarget = order.collectionAddress
+        const parsed = await looksRare.parseLooksRareOrder(order)
+
+        if (parsed) {
+          parsedOrders.push(parsed)
+        }
+
+        values.push({
+          hash: order.hash,
+          target: orderTarget.toLowerCase(),
+          maker: order.signer,
+          created_at: new Date(Number(order.startTime)),
+          data: order as any,
+          source: 'looksrare',
+        })
+      }
+
+      // const plimit = pLimit(20)
+      await Promise.all(orders.map(order => handleOrder(order)))
+      console.log({
+        parsedOrders,
+      })
+
+      if (values.length) {
+        // insert to database
+        // empty result if all transactions already exist,
+        // return most recent order hash
+      }
+
+      // if backfill, log result length
+      // if parsedOrders exist, addToRelayOrdersQueue
+
+      // wait to avoid rate-limiting
+      await new Promise(resolve => setTimeout(resolve, 1000))
+      return parsedOrders
+    } catch (error) {
+      console.log(error)
+    }
+
+    return url
   }
 
   async testKit() {
