c6f014517b56edbdfc405be4787a19b2cf05bc9b
fkymy <yuskefuku@gmail.com>
2022-11-05 20:12:48 +0900

    add bull and redis

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,3 +1,4 @@
+import { BullModule } from '@nestjs/bull'
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { ScheduleModule } from '@nestjs/schedule'
@@ -19,6 +20,12 @@ import { UserModule } from './user/user.module'
       load: [config],
     }),
     ScheduleModule.forRoot(),
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
     AuthModule,
     UserModule,
     PrismaModule,
